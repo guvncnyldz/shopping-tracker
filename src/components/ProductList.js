236498deb43e2f1aa15afc7856c544e6b4fec757
Güven Can Yıldız <yildiz.guvencan@gmail.com>
2025-02-15 11:37:11 +0300

    In home feature

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,18 +5,18 @@ import { Loading } from './Loading';
 export default function ProductList() {
     const [products, setProduct] = useState([])
     const [isLoading, setLoading] = useState(false)
-    const [isFiltered, setFiltered] = useState(false)
+    const [filterType, setFilterType] = useState(0)
 
     useEffect(() => {
         getProductsBegin();
     }, []);
 
-    async function handleFiltered(filtered) {
-        if (filtered == isFiltered)
+    async function handleFiltered(targetFilterType) {
+        if (filterType == targetFilterType)
             return
 
         await getProductsBegin()
-        setFiltered(filtered)
+        setFilterType(targetFilterType)
     }
 
     async function getProductsBegin() {
@@ -34,18 +34,21 @@ export default function ProductList() {
         <div className="mt-4 space-y-2">
             <div className='flex flex-row gap-4'>
                 <button
-                    className={`flex justify-center items-center w-1/2 h-12 rounded shadow-md p-4 ${!isFiltered ? "bg-blue-200" : "bg-white"}`}
-                    onClick={() => { handleFiltered(false) }}>TÜM ÜRÜNLER</button>
+                    className={`flex justify-center items-center w-1/3 h-12 rounded shadow-md p-4 ${filterType == 0 ? "bg-blue-200" : "bg-white"}`}
+                    onClick={() => { handleFiltered(0) }}>TÜM ÜRÜNLER</button>
                 <button
-                    className={`flex justify-center items-center w-1/2 h-12 rounded shadow-md p-4 ${isFiltered ? "bg-blue-200" : "bg-white"}`}
-                    onClick={() => handleFiltered(true)}>SEPETTEKİLER</button>
+                    className={`flex justify-center items-center w-1/3 h-12 rounded shadow-md p-4 ${filterType == 1 ? "bg-blue-200" : "bg-white"}`}
+                    onClick={() => handleFiltered(1)}>EVDEKİLER</button>
+                <button
+                    className={`flex justify-center items-center w-1/3 h-12 rounded shadow-md p-4 ${filterType == 2 ? "bg-blue-200" : "bg-white"}`}
+                    onClick={() => handleFiltered(2)}>SEPETTEKİLER</button>
             </div>
             <div className='bg-white rounded shadow-md p-4'>
                 <h2 className="text-lg font-semibold mb-2">Ürünler</h2>
                 {isLoading ? <Loading width={10} height={10} /> :
                     products.length == 0 ? <div className='text-center'>Ürün Bulunamadı</div> :
                         products.map((product) => {
-                            if (!isFiltered || (isFiltered && product.is_required))
+                            if (filterType == 0 || (filterType == 1 && !product.is_required) || (filterType == 2 && product.is_required))
                                 return (<ProductItem productId={product.product_id} product={product} onProductRemoved={onProductRemoved} />)
                         })
                 }
